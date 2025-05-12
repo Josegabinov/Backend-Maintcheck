f94260e451e463cb7e24ab8d8640ad83b106bcdc
josegabinov <jfgabinov@uqvirtual.edu.co>
2025-05-12 14:24:29 -0500

    modify settings

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-4c#vjpjdcc+zxwn1##90t#-=^rz#m!0or40747fpjsj4s!^o%r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backend-maintcheck-1.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
